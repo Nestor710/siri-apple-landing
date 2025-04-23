b98d8df357294a568d375b7a270cdcbb666cb522
Nestor710 <nestorlima710@gmail.com>
2025-04-23 00:30:00 -0500

    preact added to the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,17 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  experimental: {
+    svg: true
+  },
+
+  integrations: [preact({ compat: true })]
 });
